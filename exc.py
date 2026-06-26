bf97635168c4bd14e52691e0731c2570823a9038
Pranay <pranayvjadhao197@gmail.com>
2026-06-26 23:34:59 +0530

    Initial commit

---
+++ exc.py
@@ -0,0 +1,10 @@
+print("Program starts...")
+
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+c = a // b  # Integer division like Java
+
+print("The division of", a, "and", b, "is", c)
+
+print("Program ends...")
